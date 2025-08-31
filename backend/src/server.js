cef05eacb552d86e767c6da189e17e7f729419ea
Ashithdeveloper <ashithashith593@gmail.com>
2025-08-31 19:28:24 +0530

    go deploy update

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,10 +38,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+    connectDB();
  if (ENV.NODE_ENV !== "production") {
   app.listen(ENV.PORT, () => {
     console.log("Server is running on port 5000");
-    connectDB();
   });
  }
 
